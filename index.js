d9589150c468adaeb7cb9f833462456a1608c52a
bulatsh <bulatshfglh@gmail.com>
2014-06-23 00:11:27 +0400

    bugfix: webpack crashes if error happens during processing file in sweetjs

--- index.js
+++ index.js
@@ -6,26 +6,30 @@ module.exports = function(source) {
   this.async();
 
   var config = loaderUtils.parseQuery(this.query);
+  var current_file = loaderUtils.getRemainingRequest(this)
 
   async.map(
     config.modules || [],
     function(module, callback) {
+      var _this = this;
       this.resolve(this.context, module, function(err, result) {
-	if (err) {
-	  callback(err);
-	  return;
-	}
-	try {
-	  callback(null, sweet.loadNodeModule(process.cwd(), result));
-	} catch (e) {
-	  callback(e);
-	}
+        if (err) {
+          callback(err);
+          return;
+        }
+        try {
+          callback(null, sweet.loadNodeModule(process.cwd(), result));
+        } catch (e) {
+          var err = "Sweet.js compile error: " + e.description + " in " + current_file + "(" + e.lineNumber + ":" + e.column + ")"
+          console.log(err)
+          _this.callback(null, "throw new Error('" + err + "')", "")
+        }
       });
     }.bind(this),
     function(err, results) {
       if (err) {
-	this.callback(err);
-	return;
+        this.callback(err);
+        return;
       }
       config.modules = results;
       var result = sweet.compile(source, config);
